90a5e698701d9b3bd8d2527c9db829b246e24db8
Carlos <160151768+CastiellC@users.noreply.github.com>
2024-06-14 12:28:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+/*var a = 17;
+var b = 24;
+
+var c = a + b;
+
+console.log(c) ;
+
+var d = 17;
+var e = 24;
+
+var f = d + e;
+
+console.log(c) ;
+
+
+var g = 17;
+var h = 24;
+
+var i = g / h;
+
+console.log(c) ;
+
+
+
+var j = 17;
+var k = 24;
+
+var l = j - k;
+
+console.log(c) ;
+
+
+
+var m = 17;
+var ç = 24;
+
+var o = m ++ ç ;
+
+console.log(c) ;
+
+
+var p = 17;
+var z = 24;
+
+var r = p -- z ;
+
+console.log(c) ;
+
+var s = 17;
+var t = 24;
+
+var u = s % t;
+
+console.log(c);*/
